fc613fddd620e635eda0c7d59158d23feec57cac
dbibb99 <dbibb99@gmail.com>
2025-06-18 11:35:28 -0400

    added char count feature, report, ability to analyze other books

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-from stats import get_num_words
+from stats import get_num_words, get_num_char, get_char_list
+import sys
 
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(get_num_words(file_contents))     
+    if len(sys.argv) > 1:
+        file_path = sys.argv[1]
+        with open(file_path) as f:
+            file_contents = f.read()
+            num_words = get_num_words(file_contents)
+            num_char = get_num_char(file_contents)
+            sorted_num_char = get_char_list(num_char)  
+
+            print(f"============ BOOKBOT ============\nAnalyzing book found at {file_path}...\n------------ Word Count ------------\nFound {num_words} total words\n------------ Character Count ------------")
+            for dict in sorted_num_char:
+                print(f"{dict['char']}: {dict['num']}")
+            print("============ END ============")
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
 if __name__=="__main__":
     main()
--- stats.py
+++ stats.py
@@ -1,3 +1,24 @@
 def get_num_words(file_contents):
         content_list = file_contents.split()
-        return(f"{len(content_list)} words found in the document")
+        return(len(content_list))
+
+def get_num_char(file_contents):
+        char_count = {}
+        for char in file_contents:
+                char = char.lower()
+                if char in char_count:
+                        char_count[char] += 1
+                else:
+                        char_count[char] = 1
+        return char_count
+
+def get_char_list(char_dict):
+        char_list = []
+        for char in char_dict:
+                if char.isalpha():
+                        new_dict = {}
+                        new_dict['char'] = char
+                        new_dict['num'] = char_dict[char]
+                        char_list.append(new_dict)
+        return sorted(char_list, key=lambda d: d['num'], reverse=True)
+
